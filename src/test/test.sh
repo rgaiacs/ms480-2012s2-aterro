d1365a9ca02da394e22cc8d62ffe955e143b00a1
Raniere Silva <r.gaia.cs@gmail.com>
2012-11-06 07:25:10 -0200

    Fixed bug in test/test.sh

--- src/test/test.sh
+++ src/test/test.sh
@@ -2,6 +2,27 @@
 python test/test.py --pickle --psolve --preduce 100
 python test/test.py -b 1 --pickle --psolve --preduce 100
 python test/test.py -b 2 550 421 98 --pickle --psolve --preduce 100
-python test/test.py --pickle --psolve --preduce 10
-python test/test.py -b 1 --pickle --psolve --preduce 10
-python test/test.py -b 2 550 421 98 --pickle --psolve --preduce 10
+python test/test.py --pickle --psolve --preduce 50
+python test/test.py -b 1 --pickle --psolve --preduce 50
+python test/test.py -b 2 550 421 98 --pickle --psolve --preduce 50
+python test/test.py --pickle --psolve --preduce 25
+python test/test.py -b 1 --pickle --psolve --preduce 25
+python test/test.py -b 2 550 421 98 --pickle --psolve --preduce 25
+python test/test.py --pickle --psolve --preduce 100 -f test/t1.ppm
+python test/test.py -b 1 --pickle --psolve --preduce 100 -f test/t1.ppm
+python test/test.py -b 2 550 421 98 --pickle --psolve --preduce 100 -f test/t1.ppm
+python test/test.py --pickle --psolve --preduce 50 -f test/t1.ppm
+python test/test.py -b 1 --pickle --psolve --preduce 50 -f test/t1.ppm
+python test/test.py -b 2 550 421 98 --pickle --psolve --preduce 50 -f test/t1.ppm
+python test/test.py --pickle --psolve --preduce 25 -f test/t1.ppm
+python test/test.py -b 1 --pickle --psolve --preduce 25 -f test/t1.ppm
+python test/test.py -b 2 550 421 98 --pickle --psolve --preduce 25 -f test/t1.ppm
+python test/test.py --pickle --psolve --preduce 100 -f test/t2.ppm
+python test/test.py -b 1 --pickle --psolve --preduce 100 -f test/t2.ppm
+python test/test.py -b 2 550 421 98 --pickle --psolve --preduce 100 -f test/t2.ppm
+python test/test.py --pickle --psolve --preduce 50 -f test/t2.ppm
+python test/test.py -b 1 --pickle --psolve --preduce 50 -f test/t2.ppm
+python test/test.py -b 2 550 421 98 --pickle --psolve --preduce 50 -f test/t2.ppm
+python test/test.py --pickle --psolve --preduce 25 -f test/t2.ppm
+python test/test.py -b 1 --pickle --psolve --preduce 25 -f test/t2.ppm
+python test/test.py -b 2 550 421 98 --pickle --psolve --preduce 25 -f test/t2.ppm
