5b359f83619491d3bd1f98a97b743e431e1e70d8
Raniere Silva <r.gaia.cs@gmail.com>
2012-11-22 21:12:00 -0200

    Add the csv files for resultados.

--- src/test/test_analyse.sh
+++ src/test/test_analyse.sh
@@ -1,45 +1,29 @@
-echo '800-100'
-sqlite3 test.db "SELECT dtype, cols, rows, f_max, p_time, s_time FROM benchmark
-WHERE problem='test/t1.ppm' AND maxd=800 AND reduce=100 AND solver='aterro';"
-echo '900-100'
-sqlite3 test.db "SELECT dtype, cols, rows, f_max, p_time, s_time FROM benchmark
-WHERE problem='test/t1.ppm' AND maxd=900 AND reduce=100 AND solver='aterro';"
-echo '1000-100'
-sqlite3 test.db "SELECT dtype, cols, rows, f_max, p_time, s_time FROM benchmark
-WHERE problem='test/t1.ppm' AND maxd=1000 AND reduce=100 AND solver='aterro';"
-echo 'r800-100'
-sqlite3 test.db "SELECT dtype, cols, rows, f_max, p_time, s_time FROM benchmark
-WHERE problem='test/t1.ppm' AND maxd=800 AND reduce=100 AND solver='raterro';"
-echo 'r900-100'
-sqlite3 test.db "SELECT dtype, cols, rows, f_max, p_time, s_time FROM benchmark
-WHERE problem='test/t1.ppm' AND maxd=900 AND reduce=100 AND solver='raterro';"
-echo 'r1000-100'
-sqlite3 test.db "SELECT dtype, cols, rows, f_max, p_time, s_time FROM benchmark
-WHERE problem='test/t1.ppm' AND maxd=1000 AND reduce=100 AND solver='raterro';"
-echo 'r800-100'
-sqlite3 test.db "SELECT dtype, cols, rows, f_max, p_time, s_time FROM benchmark
-WHERE problem='test/t1.ppm' AND maxd=800 AND reduce=100 AND solver='aaterro';"
-echo 'r900-100'
-sqlite3 test.db "SELECT dtype, cols, rows, f_max, p_time, s_time FROM benchmark
-WHERE problem='test/t1.ppm' AND maxd=900 AND reduce=100 AND solver='aaterro';"
-echo 'r1000-100'
-sqlite3 test.db "SELECT dtype, cols, rows, f_max, p_time, s_time FROM benchmark
-WHERE problem='test/t1.ppm' AND maxd=1000 AND reduce=100 AND solver='aaterro';"
-echo 'r800-100'
-sqlite3 test.db "SELECT dtype, cols, rows, f_max, p_time, s_time FROM benchmark
-WHERE problem='test/t1.ppm' AND maxd=800 AND reduce=100 AND solver='aaterro';"
-echo 'r800-50'
-sqlite3 test.db "SELECT dtype, cols, rows, f_max, p_time, s_time FROM benchmark
-WHERE problem='test/t1.ppm' AND maxd=800 AND reduce=50 AND solver='aaterro';"
-echo 'r900-100'
-sqlite3 test.db "SELECT dtype, cols, rows, f_max, p_time, s_time FROM benchmark
-WHERE problem='test/t1.ppm' AND maxd=900 AND reduce=100 AND solver='aaterro';"
-echo 'r900-50'
-sqlite3 test.db "SELECT dtype, cols, rows, f_max, p_time, s_time FROM benchmark
-WHERE problem='test/t1.ppm' AND maxd=900 AND reduce=50 AND solver='aaterro';"
-echo 'r1000-100'
-sqlite3 test.db "SELECT dtype, cols, rows, f_max, p_time, s_time FROM benchmark
-WHERE problem='test/t1.ppm' AND maxd=1000 AND reduce=100 AND solver='aaterro';"
-echo 'r1000-50'
-sqlite3 test.db "SELECT dtype, cols, rows, f_max, p_time, s_time FROM benchmark
-WHERE problem='test/t1.ppm' AND maxd=1000 AND reduce=50 AND solver='aaterro';"
+echo "max. distancia,rows,cols,value,process,solver,rows,cols,value,process,solver" > maxd.csv
+sqlite3 test.db "SELECT a.maxd, a.rows, a.cols, a.f_max, ROUND(a.p_time, 4),
+ROUND(a.s_time, 4), b.rows, b.cols, b.f_max, ROUND(b.p_time, 4),
+ROUND(b.s_time, 4) FROM (SELECT maxd, rows, cols, f_max, p_time, s_time FROM benchmark
+WHERE problem='test/t1.ppm' AND reduce=100 AND solver='aaterro' AND
+dtype=2) a JOIN (SELECT maxd, rows, cols, f_max, p_time, s_time FROM benchmark
+WHERE problem='test/t2.ppm' AND reduce=100 AND solver='aaterro' AND
+dtype=2) b ON a.maxd = b.maxd;" | sed 's/|/,/g' >> maxd.csv
+echo "reduce,rows,cols,value,process,solver,rows,cols,value,process,solver" > reduce.csv
+sqlite3 test.db "SELECT a.*, b.rows, b.cols, b.f_max, b.p_time, b.s_time FROM
+(SELECT reduce, rows, cols, f_max, p_time, s_time FROM benchmark
+WHERE problem='test/t1.ppm' AND maxd=800 AND solver='aaterro' AND
+dtype=2) a JOIN (SELECT reduce, rows, cols, f_max, p_time, s_time FROM benchmark
+WHERE problem='test/t2.ppm' AND maxd=800 AND solver='aaterro' AND
+dtype=2) b ON a.reduce = b.reduce;" | sed 's/|/,/g' >> reduce.csv
+echo "solver,rows,cols,value,process,solver,rows,cols,value,process,solver" > solver.csv
+sqlite3 test.db "SELECT a.*, b.rows, b.cols, b.f_max, b.p_time, b.s_time FROM
+(SELECT solver, rows, cols, f_max, p_time, s_time FROM benchmark
+WHERE problem='test/t1.ppm' AND maxd=800 AND reduce=50 AND
+dtype=2) a JOIN (SELECT solver, rows, cols, f_max, p_time, s_time FROM benchmark
+WHERE problem='test/t2.ppm' AND maxd=800 AND reduce=50 AND
+dtype=2) b ON a.solver = b.solver;" | sed 's/|/,/g' >> solver.csv
+echo "tipo distancia,rows,cols,value,process,solver,rows,cols,value,process,solver" > dtype.csv
+sqlite3 test.db "SELECT a.*, b.rows, b.cols, b.f_max, b.p_time, b.s_time FROM
+(SELECT dtype, rows, cols, f_max, p_time, s_time FROM benchmark
+WHERE problem='test/t1.ppm' AND maxd=800 AND reduce=50 AND
+solver='aaterro') a JOIN (SELECT dtype, rows, cols, f_max, p_time, s_time FROM benchmark
+WHERE problem='test/t2.ppm' AND maxd=800 AND reduce=50 AND
+solver='aaterro') b ON a.dtype = b.dtype;" | sed 's/|/,/g' >> dtype.csv
